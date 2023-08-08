0e815c655bb9dc997f3875d702b0a0350362009d
KarpenkoIgor <hv.karpenko@gmail.com>
2023-08-08 16:12:20 +0300

    Created mock service

---
+++ Mock.Json/Controllers/JsonHandlerController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text.Encodings.Web;
+using System.Text.Json;
+using System.Text.Unicode;
+
+namespace Mock.Json.Controllers
+{
+    public class JsonHandlerController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public JsonHandlerController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet("{filename}")]
+        public async Task<IActionResult> GetJsonAsync(string filename)
+        
+        {
+            string filePath = Path.Combine(_configuration["JsonHandler:FolderPath"], $"{filename}.json");
+
+            if (!System.IO.File.Exists(filePath))
+            {
+                var errorMessage = new { message = $"File with name '{filename}' not found" };
+                return NotFound(errorMessage);
+            }
+
+            string jsonContent = await System.IO.File.ReadAllTextAsync(filePath);
+            return Content(jsonContent, "application/json");
+        }
+
+        [HttpGet("with-folder")]
+        public async Task<IActionResult> GetJsonWithFolderPathAsync(string folder, string filename)
+        {
+            var folderPath = Path.Combine(_configuration["JsonHandler:FolderPath"], folder);
+            string filePath = Path.Combine(folderPath, $"{filename}.json");
+
+            if (!Directory.Exists(folderPath) || !System.IO.File.Exists(filePath))
+            {
+                var errorMessage = new { message = $"File with name '{filename}' not found" };
+                return NotFound(errorMessage);
+            }
+
+            string jsonContent = await System.IO.File.ReadAllTextAsync(filePath);
+            return Content(jsonContent, "application/json");
+        }
+    }
+}
---
+++ Mock.Json/Program.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+using Microsoft.AspNetCore.Server.Kestrel.Https;
+using System.Security.Cryptography.X509Certificates;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+
