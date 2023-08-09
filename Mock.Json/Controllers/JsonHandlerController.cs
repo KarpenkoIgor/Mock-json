1d8bec60471330ce37a3dad843120f387ded6352
KarpenkoIgor <hv.karpenko@gmail.com>
2023-08-09 11:11:17 +0300

    Added with-folder information logging

--- Mock.Json/Controllers/JsonHandlerController.cs
+++ Mock.Json/Controllers/JsonHandlerController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
+using Serilog;
 using System.Text.Encodings.Web;
 using System.Text.Json;
 using System.Text.Unicode;
@@ -34,8 +35,17 @@ namespace Mock.Json.Controllers
         [HttpGet("with-folder")]
         public async Task<IActionResult> GetJsonWithFolderPathAsync(string folder, string filename)
         {
+            Log.Information($"folder:{folder}");
+            Log.Information($"file: {filename}");
+            Log.Information($"folderPath: {Path.Combine(_configuration["JsonHandler:FolderPath"], folder)}");
             var folderPath = Path.Combine(_configuration["JsonHandler:FolderPath"], folder);
+            Log.Information($"filePath: {Path.Combine(folderPath, $"{filename}.json")}");
             string filePath = Path.Combine(folderPath, $"{filename}.json");
+
+
+            Log.Information($"Directory exists: {Directory.Exists(folderPath)}");
+            Log.Information($"File exists: {System.IO.File.Exists(filePath)}");
+
 
             if (!Directory.Exists(folderPath) || !System.IO.File.Exists(filePath))
             {
